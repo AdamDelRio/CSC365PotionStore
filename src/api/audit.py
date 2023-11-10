0d1f09380554d75fa859b42902108cc0b095fd06
AdamDelRio <adamdelrio8523@gmail.com>
2023-11-10 14:28:38 -0800

    audit oopsie

--- src/api/audit.py
+++ src/api/audit.py
@@ -33,12 +33,12 @@ def get_inventory():
         print(str(connection.execute(sqlalchemy.text("SELECT SUM(change) FROM ml_ledger WHERE color = 'red'")).first()[0]) + " red")
         print(str(connection.execute(sqlalchemy.text("SELECT SUM(change) FROM ml_ledger WHERE color = 'green'")).first()[0]) + " green")
         print(str(connection.execute(sqlalchemy.text("SELECT SUM(change) FROM ml_ledger WHERE color = 'dark'")).first()[0]) + " dark")
-        connection.execute(sqlalchemy.text("INSERT INTO gold_ledger (entry, change, description) VALUES ('audit fix', -50, 'Fixing audit error')"))
+        #connection.execute(sqlalchemy.text("INSERT INTO gold_ledger (entry, change, description) VALUES ('audit fix', 500, 'Fixing audit error')"))
         #connection.execute(sqlalchemy.text("INSERT INTO ml_ledger (color, entry, change, description) VALUES ('green', 'reset', 1920, 'Removing all red barrels from inventory')"))
-        connection.execute(
-                sqlalchemy.text("INSERT INTO potion_ledger (entry, change, potion_id, description) VALUES (:entry, :change, :potion_id, :description)"),
-                {'entry': 'reset', 'change': 1, 'potion_id': 1, 'description': 'Removing all potions from inventory'}
-            )
+        #connection.execute(
+        #        sqlalchemy.text("INSERT INTO potion_ledger (entry, change, potion_id, description) VALUES (:entry, :change, :potion_id, :description)"),
+        #        {'entry': 'reset', 'change': -13, 'potion_id': 1, 'description': 'Removing all potions from inventory'}
+        #    )
 
     return {
         "number_of_potions": result_potions,
